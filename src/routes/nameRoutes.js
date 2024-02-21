bc24286dd5dc93a34d4ebb0b7a94fbf513ef4b16
ndoyeKevin <kevin.ndoye@institutsolacroup.com>
2024-02-21 11:13:28 +0100

    up

--- src/routes/nameRoutes.js
+++ src/routes/nameRoutes.js
@@ -1,7 +1,7 @@
 // nameRoute.js
 const express = require("express");
 const router = express.Router();
-const nameController = require("./nameController");
+const nameController = require("../controllers/nameController");
 
 // Définir la route
 router.get("/name", nameController.getName);
